44c3fa16213f6a6273d0210ddc2627c163c19580
emrebuzkiran <emre-04gs@hotmail.com>
2024-10-23 19:36:34 +0300

    Refactor stockServices.js, userController.js, and userRoutes.js

--- config.js
+++ config.js
@@ -1,24 +1,34 @@
-// config.js
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config(); // Ortam değişkenlerini kullanmak için dotenv'i dahil edin
 
 const uri = process.env.MONGO_URI; // .env dosyasındaki URI'yi kullanın
+
+// Bağlantı havuzu seçenekleri ile MongoClient oluşturun
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    maxPoolSize: 10, // Maksimum bağlantı sayısı (örneğin 10)
+    minPoolSize: 2, // Minimum bağlantı sayısı (örneğin 2)
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
+
+let dbInstance; // Bağlantıdan dönecek veritabanı örneği
+
 async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Successfully connected to MongoDB!");
-    return client.db("mydatabase"); // 'mydatabase' isimli veritabanını kullanın
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
+    if (!dbInstance) { // Eğer dbInstance yoksa
+        try {
+            await client.connect();
+            console.log("Successfully connected to MongoDB!");
+            dbInstance = client.db("mydatabase"); // 'mydatabase' isimli veritabanını kullanın
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+            throw error; // Hata fırlat
+        }
+    }
+    return dbInstance; // Veritabanı örneğini döndür
 }
 
 module.exports = { client, connectDB };
